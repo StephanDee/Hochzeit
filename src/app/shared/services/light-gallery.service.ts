70c9a8f95d88607dbb600f0fc68518d7162f3086
Stephan <duenkel.stephan@gmail.com>
2025-03-12 20:10:31 +0100

    add favicon, text adjustments

--- src/app/shared/services/light-gallery.service.ts
+++ src/app/shared/services/light-gallery.service.ts
@@ -30,11 +30,11 @@ export class LightGalleryService {
     return [
       new LightGalleryItem(
         'images/gallery/1.JPG',
-        this.convertSubtitleDataToHtml('Photo by - Stephan', 'Location - Warnemünde - Unsere erste Reise')
+        this.convertSubtitleDataToHtml('Warnemünde', 'Unsere erste Reise')
       ),
       new LightGalleryItem(
         'images/gallery/2.JPG',
-        this.convertSubtitleDataToHtml('Photo by - Yong Yong', 'Location - Warnemünde - Unsere erste Reise')
+        this.convertSubtitleDataToHtml('Warnemünde', 'Unsere erste Reise')
       ),
       /*new LightGalleryItem(
         'images/gallery/3.JPG',
@@ -42,51 +42,51 @@ export class LightGalleryService {
       ),*/
       new LightGalleryItem(
         'images/gallery/5.JPG',
-        this.convertSubtitleDataToHtml('Photo by - Stephan', 'Location - Barcelona - Zum ersten mal zusammen im Ausland')
+        this.convertSubtitleDataToHtml('Barcelona', 'Die erste Reise zusammen außerhalb Deutschland')
       ),
       new LightGalleryItem(
         'images/gallery/4.JPG',
-        this.convertSubtitleDataToHtml('Photo by - Yong Yong', 'Location - Nizza')
+        this.convertSubtitleDataToHtml('Nizza', 'Das erste mal zusammen in Frankreich')
       ),
       new LightGalleryItem(
         'images/gallery/6.JPG',
-        this.convertSubtitleDataToHtml('Photo by - Stephan', 'Location - Köln - Unser erstes Liebesschloss - Wer findet es?')
+        this.convertSubtitleDataToHtml('Köln', 'Unser erstes Liebesschloss - Wer findet es?')
       ),
       new LightGalleryItem(
         'images/gallery/7.JPG',
-        this.convertSubtitleDataToHtml('Photo by - Stephan', 'Location - Time Square - Unsere erste USA Reise nach New York')
+        this.convertSubtitleDataToHtml('New York', 'Unsere erste USA Reise in New York - Time Square')
       ),
       new LightGalleryItem(
         'images/gallery/8.JPG',
-        this.convertSubtitleDataToHtml('Photo by - Stephan', 'Location - New York - Das erste mal im Central Park')
+        this.convertSubtitleDataToHtml('New York', 'Das erste mal im Central Park')
       ),
       new LightGalleryItem(
         'images/gallery/9.JPG',
-        this.convertSubtitleDataToHtml('Photo by - Hanh', 'Location - Hong Kong - Unsere große China Reise')
+        this.convertSubtitleDataToHtml('Hong Kong', 'Unsere große China Reise')
       ),
       new LightGalleryItem(
         'images/gallery/10.JPG',
-        this.convertSubtitleDataToHtml('Photo by - Yong Yong', 'Location - Fuertaventura - Unser erster Roadtrip')
+        this.convertSubtitleDataToHtml('Fuertaventura', 'Unser erster Roadtrip')
       ),
       new LightGalleryItem(
         'images/gallery/11.jpg',
-        this.convertSubtitleDataToHtml('Photo by - Stephan', 'Location - Fuertaventura - Auf den Dünen')
+        this.convertSubtitleDataToHtml('Fuertaventura', 'Auf den Dünen Fuertaventuras')
       ),
       new LightGalleryItem(
         'images/gallery/12.jpg',
-        this.convertSubtitleDataToHtml('Photo by - Yong Yong', 'Location - Death Valley - Großer Roadtrip auf der West Küste der USA')
+        this.convertSubtitleDataToHtml('Death Valley', 'Großer Roadtrip auf der West Küste der USA')
       ),
       new LightGalleryItem(
         'images/gallery/13.jpg',
-        this.convertSubtitleDataToHtml('Photo by - Yong Yong', 'Location - Singapur - Marina Bay')
+        this.convertSubtitleDataToHtml('Singapur', 'Marina Bay')
       ),
       new LightGalleryItem(
         'images/gallery/15.jpg',
-        this.convertSubtitleDataToHtml('Photo by - Yong Yong', 'Location - Korea - Tempelbesichtung im Hambok')
+        this.convertSubtitleDataToHtml('Korea', 'Tempelbesichtung im Hambok')
       ),
       new LightGalleryItem(
         'images/gallery/16.jpg',
-        this.convertSubtitleDataToHtml('Photo by - Yong Yong', 'Location - Korea - Unser Liebesschloss am Namsan Tower')
+        this.convertSubtitleDataToHtml('Korea', 'Unser Liebesschloss am Namsan Tower')
       )
     ];
   }
